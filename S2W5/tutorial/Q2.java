c8393f3f1030eab14125fe4507d28ff8d4d2533a
ZhengWei <zhengwei0409@gmail.com>
2024-04-07 13:07:16 +0800

    week5.1

---
+++ S2W5/tutorial/Q2.java
@@ -0,0 +1,22 @@
+package S2W5.tutorial;
+
+public class Q2 {
+    /*
+     * a)
+     * Summary: Remove the (index)th node from a doubly linked list.
+     * Line 2 - 5:
+     * Set temp to point to the (index)th node to be removed.
+     * Line 6:
+     * Assign the element of temp to element, to be returned later.
+     * Line 7:
+     * Set prev of the node after temp to the node before temp (skipping the node to be removed).
+     * Line 8:
+     * Set next of the node before temp to the node after temp (skipping the node to be removed).
+     * Line 9 - 10:
+     * (Unnecessarily) Set the next and prev of temp to null.
+     * Line 11:
+     * Decrement size by 1 as one node is removed.
+     */
+
+     
+}
