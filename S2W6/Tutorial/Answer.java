02e9b50fdbb41f623dfc64e1627d76767a12c37f
ZhengWei <zhengwei0409@gmail.com>
2024-04-17 11:42:41 +0800

    Updated W6 tuto

---
+++ S2W6/Tutorial/Answer.java
@@ -0,0 +1,49 @@
+package S2W6.Tutorial;
+
+public class Answer {
+    // Question 1
+    // C. peek
+
+    // Question 2
+    // S1 - empty , S2 - one | two , S3 - empty
+
+    // Question 3
+    // a) Yes
+    //    stack2.push(stack.pop());
+    //    stack2.push(stack.pop());
+    //    stack.pop();
+    //    stack2.pop();
+    //    stack2.pop();
+
+    // b) Yes
+    //    stack_3.push(stack.pop());
+    //    stack.pop();
+    //    stack_3.pop();
+    //    stack.pop();
+
+    // c) Yes 
+    //    stack.pop();
+    //    stack.pop();
+    //    stack.pop();
+
+    // d) Yes 
+    //    stack_3.push(stack.pop());
+    //    stack_2.push(stack.pop());
+    //    stack.pop();
+    //    stack_3.pop();
+    //    stack_2.pop();
+
+
+    // Question 4
+    // a) a b c * +
+    // b) a b * c d / -
+    // c) a b c * d + e / + 
+
+    // Question 5
+    // a) (a+b)*c
+    // b) a*(b+c)
+
+    // Question 6
+    // all of the above
+
+}
