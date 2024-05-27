ea5083224677eac6c3df3f521ff86233e56d5d77
ZhengWei <zhengwei0409@gmail.com>
2024-05-27 15:12:37 +0800

    updated tuto12

---
+++ S2W12/tuto/Q1.java
@@ -0,0 +1,12 @@
+package S2W12.tuto;
+
+public class Q1 {
+
+    /*
+     *  A binary search tree (BST) is a way to organize a bunch of data where each item has a value, and the tree helps you quickly find a specific item by comparing its value to the values of the items in the tree.
+        BST is called "binary" because each item can have at most two child items.
+        BST is organized so that each item's value is greater than the value of its left child, and less than the value of its right child.
+        This makes it easy to narrow down your search for a specific item as you move through the tree.
+
+     */
+}
---
+++ S2W12/tuto/Q3.java
@@ -0,0 +1,5 @@
+package S2W12.tuto;
+
+public class Q3 {
+    // 5
+}
